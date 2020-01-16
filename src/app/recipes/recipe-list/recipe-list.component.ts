3381e8811bbf9e88f5a99acbcff80b1c1f4037a9
Muhammad Waqar <mohammadwaqar401@gmail.com>
2020-01-16 16:03:28 +0500

    added recipe list feature

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] =[
+    new Recipe('A Test Recipe ', 'This is simple test', 'https://images.unsplash.com/photo-1455619452474-d2be8b1e70cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'),
+    new Recipe('A Test Recipe ', 'This is simple test', 'https://images.unsplash.com/photo-1552914343-54bcc3ba07f8?ixlib=rb-1.2.1&auto=format&fit=crop&w=976&q=80'),
+  
+  ];
   constructor() { }
 
   ngOnInit() {
